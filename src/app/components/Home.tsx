6841a14782a944c3836a56dbfddef13d9a197f5e
Anurag30112003 <anuragsharma011011@gmail.com>
2023-12-16 06:54:53 +0530

    feat: fetch all tools in homepage

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,7 +1,55 @@
 "use client"
+import { useState, useEffect } from "react";
+import axios from "axios";
+import CardComponent from "./Card";
 
-export default function Home() {    
+interface Company {
+  id: string;
+  name: string;
+  description: string;
+  url: string;
+  githubUrl: string;
+  imageUrl: string;
+  createdAt: string;
+  closedSrc: ClosedSource[];
+}
+
+interface ClosedSource {
+  id: string;
+  name: string;
+  imageUrl: string;
+  createdAt: string;
+}
+
+export default function Home() {
+  const [data, setData] = useState<Company[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<Company[]>('/api/post/getAll');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      {data.map((item) => (
+        <CardComponent
+          key={item.id}
+          name={item.name}
+          imageUrl={item.imageUrl}
+          description={item.description}
+          url={item.url}
+          githubUrl={item.githubUrl}
+          closedSrcName={item.closedSrc[0].name}
+        />
+      ))}
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
+import HomeComponent from './components/Home'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between">
       <div>
-        Hello
+        <HomeComponent />
       </div>
     </main>
   )
